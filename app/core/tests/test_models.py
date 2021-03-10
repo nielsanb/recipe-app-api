90c783464f5e67fb7ec04b17a4e509fe3881b0d3
Niels Github <niels.anbeek@live.nl>
2021-03-10 17:22:36 +0100

    added user and superuser TDD-way

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -25,3 +25,15 @@ class ModelTests(TestCase):
     def test_testing(self):
         """Tests if tests are run. Inception ..."""
         self.assertEqual(1,1)
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser('test@niels.io','test123')
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
